2cea527b562a26aa9619d1e2c6c8987043cd8348
Paulino Jr <paulino@quicktech.no>
2018-04-29 23:06:21 -0300

    Mudei o nome do tema e adicionei o slider

---
+++ wp-content/themes/bragantec/footer.php
@@ -0,0 +1,17 @@
+<footer>
+  <div class="row">
+    <div class="four columns">four</div>
+    <div class="four columns">four</div>
+    <div class="four columns">four</div>
+  </div>
+</footer>
+
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/jScript.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.5/js/swiper.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.5/js/swiper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.5/js/swiper.min.js.map"></script>
+
+
+</body>
+</html>
